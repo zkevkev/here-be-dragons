71baf1735ed9f13878550fd84212a0ade503ffd1
Kevin Zolman <kevin.zolman@gmail.com>
2023-11-22 06:55:21 -0700

    Complete test debug

--- lib/nytimes.rb
+++ lib/nytimes.rb
@@ -1,4 +1,4 @@
-require 'awesome_print'
+# require 'awesome_print'
 
 class Nytimes
   DATA = {
--- spec/nytimes_spec.rb
+++ spec/nytimes_spec.rb
@@ -15,21 +15,27 @@ class Nytimes
       expect(result).to eq("OK")
     end
 
-    xit 'can get copyright' do 
+    it 'can get copyright' do 
       #Using @hash, define a variable called `result` that returns the copyright
+      result = @hash[:copyright]
 
       expect(result).to eq("Copyright (c) 2018 The New York Times Company. All Rights Reserved.")
     end
 
-    xit 'can get array of stories' do 
+    it 'can get array of stories' do 
       #Using @hash, define a variable called `result` that returns the array of stories
+      result = @hash[:results]
   
       expect(result).to be_an_instance_of(Array)
       expect(result.count).to eq(44)
     end
 
-    xit 'can get all stories with subsection of politics' do 
+    it 'can get all stories with subsection of politics' do 
       #Using @hash, define a variable called `result` that returns all stories with subsection of politics.
+      result = []
+      @hash[:results].each do |story|
+        result << story if story[:subsection] == "Politics"
+      end
   
       expect(result).to be_an_instance_of(Array)
       expect(result.count).to eq(6)
